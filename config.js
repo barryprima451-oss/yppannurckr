2c51cd4ada83851f3136b68ca459c8337366dc94
barryprima451-oss <barryprima451@gmail.com>
2025-11-30 07:38:34 +0700

    Add files via upload

--- config.js
+++ config.js
@@ -1,3 +1,15 @@
 // Konfigurasi versi konten
 // Ubah angka berikut SETIAP kali menambahkan berita baru
 const CURRENT_NEWS_VERSION = 4;
+
+
+// NONAKTIFKAN AKSI COPY & SELEKSI TEKS DI SELURUH HALAMAN
+document.addEventListener('copy', function(e) {
+    e.preventDefault();
+});
+document.addEventListener('cut', function(e) {
+    e.preventDefault();
+});
+document.addEventListener('contextmenu', function(e) {
+    e.preventDefault();
+});
